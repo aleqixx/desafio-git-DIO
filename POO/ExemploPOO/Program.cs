48619ff7d53d067fd248afbf115a161cfe63546d
aleqixx <alex.rodrigues.cce@gmail.com>
2022-01-13 10:58:11 -0300

    Manipulação de Arquivos

--- POO/ExemploPOO/Program.cs
+++ POO/ExemploPOO/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExemploPOO.Helper;
 using ExemploPOO.Interfaces;
 using ExemploPOO.Models;
 
@@ -8,8 +11,34 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         {
-            ICalculadora calc = new Calculadora();
-            System.Console.WriteLine(calc.Dividir(10,5));
+            var caminho = "D:\\OneDrive - Personal\\OneDrive\\T. I\\Developer\\Bootcamp .NET Localiza\\Git\\desafio-git-DIO\\TrabalhandoComArquivos";
+            var caminhoPathCombine = Path.Combine(caminho, "Pasta teste 1");            
+            var caminhoArquivo = Path.Combine(caminho, "arquivo-teste-stream.txt");
+            var caminhoArquivoTeste = Path.Combine(caminho, "arquivo-teste.txt");
+            var caminhoArquivoTesteCopia = Path.Combine(caminho, "arquivo-teste-bkp.txt");
+            var novoCaminhoArquivo = Path.Combine(caminho, "Pasta teste 2", "arquivo-teste-stream.txt");
+
+            var listaString = new List<string> {"Linha 1", "Linha 2", "Linha 3"};
+            var listaStringContinuacao = new List<string> {"Linha 4", "Linha 5", "Linha 6"};
+
+            FileHelper helper = new FileHelper();
+            // helper.ListarDiretorios(caminho);
+            // helper.ListarArquivosDiretorios(caminho);
+
+            // System.Console.WriteLine("Criando diretório: " + caminhoPathCombine);
+            // helper.CriarDiretorio(caminhoPathCombine);
+            // helper.ApagarDiretorio(caminhoPathCombine, true);
+
+            // helper.CriarArquivoTexto(caminhoArquivo, "Hello world!" + "\n" +  "Teste de escrita de arquivo");
+            // helper.CriarArquivoTextoStream(caminhoArquivo, listaString);
+            // helper.AdicionarTextoStream(caminhoArquivo, listaStringContinuacao);
+            // helper.LerArquivoStream(caminhoArquivo);
+            // helper.MoverArquivo(caminhoArquivo, novoCaminhoArquivo, false);
+            // helper.CopiarArquivo(caminhoArquivoTeste, caminhoArquivoTesteCopia, false);
+            helper.DeletarArquivo(caminhoArquivoTesteCopia);
+
+            // ICalculadora calc = new Calculadora();
+            // System.Console.WriteLine(calc.Dividir(10,5));
 
             // Computador comp = new Computador();
             // System.Console.WriteLine(comp.ToString());
